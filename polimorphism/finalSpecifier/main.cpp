ac483c988d2766b8d373891e57aae1a6d1d27d5f
wwesleyaraujo <wesleyfurnas@gmail.com>
2022-04-13 20:57:43 -0300

    prevent overriding using final

---
+++ polimorphism/finalSpecifier/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class A{
+    public:
+        virtual void something(){
+            std::cout << " Do something" <<std::endl;
+        }
+        ~A();
+};
+
+class B : public A {
+    public:
+        virtual void something() final{
+            std::cout << " Do something" <<std::endl;
+        }
+        ~B();
+};
+
+class C : public B {
+    public:
+        virtual void something() {
+            std::cout << " Do something" <<std::endl;
+        }
+        ~C();
+};
+
+int main(){
+    
+}
